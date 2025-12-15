c9c45904ff0b068d302dc18e1e779a73fb63124d
totoba246 <inf25008@lehre.dhbw-stuttgart.de>
2025-12-15 16:30:52 +0100

    add lecture notes and math comp solution

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -2,8 +2,38 @@
 
 int main()
 {
-    printf("Hello, World");
+    //definition
+
+    int z_1 = aufgabe1(-2,-3);
+    int z_2 = aufgabe2(-2,-3);
+    int z_3 = aufgabe3(-2,-3);
+
+    //output
+
+    printf("Aufgabe 1: %d \n ", z_1);
+    printf("Aufgabe 2: %d \n ", z_2);
+    printf("Aufgabe 3: %d \n ", z_3);
     return 0;
+}
+
+int aufgabe1 (int x, int y){
+    int z;
+    z= x*x + y+y -(x*y) + 2;
+    return z;
+}
+
+int aufgabe2 (int x, int y){
+    int z;
+    z= (x-y)*(x-y)*(x-y)-3;
+    return z;
+}
+//3. $z_3 = \frac{2 * x^3 - \frac{1}{2} * x^2 - x + 4}{y}$
+int aufgabe3 (int x, int y){
+    int z;
+    
+    int zaehelr = 2;
+
+    return z;
 }
 
 
