4863b41d152b09df0d8b012a8109634fe247ddf5
totoba246 <inf25008@lehre.dhbw-stuttgart.de>
2025-12-15 16:53:51 +0100

    Exercises: Time Converter and Math Computations

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 
+float aufgabe1(float x, float y);
+float aufgabe2(float x, float y);
+float aufgabe3(float x, float y);
+
 int main()
 {
     //definition
 
-    int z_1 = aufgabe1(-2,-3);
-    int z_2 = aufgabe2(-2,-3);
-    int z_3 = aufgabe3(-2,-3);
+    float z_1 = aufgabe1(-2.0,-3.0);
+    float z_2 = aufgabe2(-2.0,-3.0);
+    float z_3 = aufgabe3(-2.0,-3.0);
 
     //output
 
-    printf("Aufgabe 1: %d \n ", z_1);
-    printf("Aufgabe 2: %d \n ", z_2);
-    printf("Aufgabe 3: %d \n ", z_3);
+    printf("Aufgabe 1: %f \n", z_1);
+    printf("Aufgabe 2: %f \n", z_2);
+    printf("Aufgabe 3: %f \n", z_3);
     return 0;
 }
 
-int aufgabe1 (int x, int y){
-    int z;
-    z= x*x + y+y -(x*y) + 2;
+float aufgabe1 (float x, float y){
+    float z;
+    z= ((x*x) + (y*y)) -(x*y) + 2;
     return z;
 }
 
-int aufgabe2 (int x, int y){
+float aufgabe2 (float x, float y){
     int z;
     z= (x-y)*(x-y)*(x-y)-3;
     return z;
 }
-//3. $z_3 = \frac{2 * x^3 - \frac{1}{2} * x^2 - x + 4}{y}$
-int aufgabe3 (int x, int y){
-    int z;
-    
-    int zaehelr = 2;
 
+float aufgabe3 (float x, float y){
+    float z;
+    int zaeheler = (2*(x*x*x))- 0.5*(x*x)-x+4;
+    z= zaeheler/y;
     return z;
 }
 
