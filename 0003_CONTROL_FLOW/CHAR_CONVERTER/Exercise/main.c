f1cdc39e84cae284f355d49db2efb0b07ca2f4a2
totoba246 <inf25008@lehre.dhbw-stuttgart.de>
2026-01-05 12:27:16 +0100

    dont know

--- 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/main.c
+++ 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
 {
@@ -9,18 +10,39 @@ int main()
     scanf("%c", &character);
 
     // is numeric
-
-    // is upper case
-
-    // is lower case
+    if(isdigit(character)){
+        printf("\n The Character is numeric");
+    }
 
     // is alpha
+    if(isalpha(character)){
+        printf("\n The Character is Alpha ");
+    }
 
-    // is alpha numeric
+     // is alpha numeric
+     if(isalnum(character)){
+        printf("\n The Character is Alphanumeric ");
+    }
+
+    // is upper case
+    if(isupper(character)){
+        printf("\n The Character is uppercase");
+    }
+
+    // is lower case
+    if(islower(character)){
+        printf("\n The Character is lowercase");
+    }
 
     // to upper case
+    if(isalpha(character) && islower(character)){
+        char charUP = toupper(character);
+        printf("\n The Uppercase version of %c is %c", character, charUP);
+    }
 
     // to lower case
-
-    return 0;
+      if(isalpha(character) && isupper(character)){
+        char charUP = tolower(character);
+        printf("\n The Lowercase version of %c is %c", character, charUP);
+    }
 }
--- 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
+++ 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
@@ -2,8 +2,68 @@
 
 int main()
 {
+
+      enum lanes{
+        LEFT,
+        RIGHT,
+        CENTER,
+        UNKNOWN
+    };
+
+    int speed;
+    int lane;
     printf("Create the properties of a vehicle.\n");
 
+    printf("Speed in m/s: ");
+    scanf("%d", &speed);
+
+    printf("Lane (1-Left, 2-Center, 3-Right): ");
+    scanf("%d", &lane);
+    switch (lane){
+        case  1:{
+            lane =  LEFT;
+            break;
+        }
+        case 2:{
+            lane = CENTER;
+            break;
+        }
+        case 3:{
+            lane = RIGHT;
+            break;
+        }
+        default:{
+            printf("No Lane found");
+            break;
+        }
+    }
+    
+
+    printf("\n");
+    printf("\n");
+    printf("\t \t \t \tL\t \t \t \tC\t \t  \t \tR\n");
+    printf("\t \t | \t \t \t \t | \t \t \t \t| \t \t  \t \t |\n");
+
+  switch(lane){
+    case LEFT:{
+        printf("\t \t | \t \tX\t \t | \t \t \t \t| \t \t \t \t |\n");
+        break;
+    }
+    case CENTER:{
+        printf("\t \t | \t \t \t \t | \t \tX\t \t| \t \t \t \t |\n");
+        break;
+    }
+    case RIGHT:{
+        printf("\t \t | \t \t \t \t | \t \t \t \t| \t \tX\t \t |\n");
+        break;
+    }
+    default:{
+         printf("No Lane found");
+         break;  
+    }
+  }
+    
+  printf("\t \t | \t \t \t \t | \t \t \t \t| \t \t  \t \t |\n");
 
     return 0;
 }
