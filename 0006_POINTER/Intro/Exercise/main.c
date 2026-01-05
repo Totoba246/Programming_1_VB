f4604ee8d2ad35aebca352917156ca71adad07fe
totoba246 <inf25008@lehre.dhbw-stuttgart.de>
2026-01-05 16:36:04 +0100

    pointer task

--- 0006_POINTER/Intro/Exercise/main.c
+++ 0006_POINTER/Intro/Exercise/main.c
@@ -2,7 +2,22 @@
 
 int main()
 {
-    // YOUR CODE HERE //
+    int aVal = 42;
+    int *myPtr = &aVal;
+
+    printf("Initial Value: \t\t\t\t\t %d", aVal);
+    printf("\nValue trough Pointer: \t\t\t\t %d", *myPtr);
+
+    *myPtr = 20;
+    printf("\nModified value of aValue through myPointer: \t %d", *myPtr);
+
+    int *yourPtr = myPtr;
+
+    printf("\n");
+
+    printf("\nAdress of aValue:\t\t\t\t %p", &aVal);
+    printf("\nAdress of myPointer:\t\t\t\t %p", &myPtr);
+    printf("\nAdress of yourPointer:\t\t\t\t %p", &yourPtr);
 
     return 0;
 }
