db01f82bd9258caa203e2f0b24f8770798759355
totoba246 <inf25008@lehre.dhbw-stuttgart.de>
2026-01-19 13:44:56 +0100

    idk

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main(void) 
 {
+    int n;
+    printf("\nEnter the number of elements: ");
+    scanf("%d", &n);
 
+    if(n<1)
+    {
+        printf("\nInvalid input! The number of elements must be greater than zero.");
+        return 1;
+    }
 
+    int *heapArray = malloc((size_t)n *  sizeof(int));
+
+    //überprüfen ob Memory allocation erfolgreich war
+    if(!heapArray)
+    {
+            printf("\nMemory allocation failed!");
+            return 1;
+        }
+
+    printf("\nEnter %d integers: ", n);
+
+    //werte in Array schreiben
+    for(int  i = 0; i < n; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        heapArray[i] = a;
+    }
+
+    //Quersumme bilden
+    int sum = 0;
+    for(int  i = 0; i < n; i++)
+    {
+        sum += heapArray[i];
+    }
+    free(heapArray);
+    printf("\nThe sum of the Array elements is: %d", sum);
     return 0;
 }
